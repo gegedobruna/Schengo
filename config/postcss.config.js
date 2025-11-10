6be90e96ad834e5cab683967349025b9a29e9479
Gege Dobruna <gegedobruna@mailbox.unideb.hu>
2025-11-11 00:43:47 +0100

    Reorganized project structure by moving all config files to config/ folder

---
+++ config/postcss.config.js
@@ -0,0 +1,13 @@
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default {
+  plugins: {
+    tailwindcss: {
+      config: resolve(__dirname, './tailwind.config.js'),
+    },
+    autoprefixer: {},
+  },
+}
--- config/vite.config.ts
+++ config/vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import { resolve, dirname } from 'node:path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/Schengo/',
+  css: {
+    postcss: resolve(__dirname, './postcss.config.js')
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('../src', import.meta.url))
     }
   },
   test: {
